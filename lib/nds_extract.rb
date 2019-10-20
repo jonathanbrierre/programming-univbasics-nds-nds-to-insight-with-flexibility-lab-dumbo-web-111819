44031bceff703e9316a88081bbae22609b117bc5
jonathan brierre <jonathanbrierre@yahoo.com>
2019-10-20 00:24:29 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -88,7 +88,8 @@ def gross_per_studio(collection)
   while i < collection.count do 
     sub_array = []
     sub_array << collection[i][:studio] if sub_array.include?(collection[i][:studio]) => false
-    
+  end
+  sub_array
     # if collection[i][:studio] ==   a_collection[collection[i][:studio]]
       
       
